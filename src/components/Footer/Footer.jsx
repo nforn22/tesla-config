f84087914f3805e060d52e3136698fce0b50c8da
nforn22 <fornarinicolas22@gmail.com>
2025-05-29 11:25:11 +0200

    add header & footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Tesla Config • Built with React • <i className="fa-brands fa-github"></i>{' '}
+      <a
+        href="https://github.com/nforn22" 
+        target="_blank"
+        className="footer-link"
+      >
+        Made by <strong>nforn22</strong>
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <i className="fa-brands fa-tesla"></i>
+      <span className="header-title">Tesla Config</span>
+    </header>
+  );
+};
+
+export default Header;
